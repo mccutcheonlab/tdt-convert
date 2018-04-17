7cab338db7a3925c5deca781f61bb35717fd6618
Jaime McCutcheon <jem64@le.ac.uk>
2018-04-17 12:18:24 +0100

    Added files for testing and code to check inputs

---
+++ testconvert.m
@@ -0,0 +1,13 @@
+% TDT test convert - mini-script for testing TDT conversion scripts
+% In general, a similar script should be stored for each set of conversions
+% somewhere near the data folder
+
+[folder, name, ext] = fileparts(which(mfilename('fullpath')));
+
+metafile = strcat(folder, '\test_metafile_2boxes.txt');
+tankfolder = 'R:\DA_and_Reward\Shared\Scripts\THPH Tanks\';
+savefolder = 'R:\DA_and_Reward\Shared\Scripts\Matlab Files\test_output\';
+
+txtfileformat = '%s %s %s %d %s %d %d %d %d %f %s %s %s %s %s %s %s %d';
+
+TDTmasterconvert(metafile, tankfolder, savefolder, 1, 0, 2, 6, txtfileformat);
